98c294b8356c4c6ab302a6d543964047026d89c7
andylidong <andy_lidong@foxmail.com>
2020-04-01 17:49:03 +0800

    update info

--- sidebars.js
+++ sidebars.js
@@ -1,5 +1,8 @@
 module.exports = {
   someSidebar: {
-    文档: ['前端', '你不知道的JavaScript', 'Android开发者必须掌握的设计模式'],
+    文档: ['前端',
+      '你不知道的JavaScript',
+      'Android开发者必须掌握的设计模式',
+      '前端面试'],
   },
 };
